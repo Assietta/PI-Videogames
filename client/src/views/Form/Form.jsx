48750f67ece2dbdf8be3644b806d7fe5dd5501dc
Jeremias Brussino <jeremiasbrussino@gmail.com>
2023-05-31 23:56:26 -0300

    commit

--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -5,15 +5,11 @@ import React, { useEffect, useState, useRef } from 'react';
 
 const Form = () => {
   const dispatch = useDispatch();
+  const formRef = useRef(null);
   const genres = useSelector((state) => state.genres);
   const plataformas = useSelector((state) => state.plataformas);
-  const formRef = useRef(null);
   const [platformError, setPlatformError] = useState('');
   const [genreError, setGenreError] = useState('');
-
-
-
-
   const [input, setInput] = useState({
     nombre: '',
     image: '',
@@ -23,7 +19,6 @@ const Form = () => {
     rating: '',
     genero: [],
 });
-
 const [errorMessages, setErrorMessages] = useState({
     nombre: '',
     image: '',
@@ -32,7 +27,6 @@ const [errorMessages, setErrorMessages] = useState({
     plataformas: '',
     genero: '',
 });
-
 
 const handleInputChange = (event) => {
   const { name, value } = event.target;
@@ -116,51 +110,6 @@ if (name === 'genre') {
 
 const handleSubmit = (event) => {
     event.preventDefault();
-
-    const errors = {};
-
-    // Validar nombre
-    if (input.nombre.trim() === '' || input.nombre.includes(' ') || input.nombre.length > 20) {
-      errors.nombre = 'El nombre es magico';
-    }
-
-    // Validar imagen
-    if (!isValidUrl(input.image)) {
-      errors.image = 'La imagen es inválida';
-    }
-
-    // Validar rating
-    if (input.rating === '') {
-        errors.rating = 'El rating es inválido';
-    }
-
-    // Validar descripcion
-    if (input.descripcion === '') {
-      errors.descripcion = 'La descripcion es invalida';
-  }
-
-    // Validar fecha de lanzamiento
-    if (!isValidDate(input.fechaLanzamiento)) {
-      errors.fechaLanzamiento = 'La fecha de lanzamiento es inválida';
-    }
-
-    // Validar selección de plataforma
-    if (input.plataformas.length === 0) {
-      setPlatformError('Debe seleccionar al menos una plataforma');
-    }
-
-    // Validar selección de género
-    if (input.genero.length === 0) {
-      setGenreError('Debe seleccionar al menos un género');
-    }
-        
-
-    if (Object.keys(errors).length > 0) {
-      setErrorMessages(errors);
-      return;
-    }
-  
-
     const { nombre, image, descripcion, plataformas, fechaLanzamiento, rating, genero } = input;
     const videogamesData = {
       nombre,
@@ -195,11 +144,11 @@ const handleSubmit = (event) => {
   };
 
   
-    const isValidUrl = (url) => {
+const isValidUrl = (url) => {
       return url.startsWith('http://') || url.startsWith('https://');
     };
   
-    const formatRating = (rating) => {
+const formatRating = (rating) => {
       let formattedRating = parseFloat(rating).toFixed(2);
       if (formattedRating.length > 4) {
         formattedRating = formattedRating.slice(0, 4);
@@ -207,7 +156,7 @@ const handleSubmit = (event) => {
       return formattedRating;
     };
   
-    const isValidDate = (date) => {
+const isValidDate = (date) => {
       return /^\d{4}-\d{2}-\d{2}$/.test(date);
   };
 
@@ -273,109 +222,83 @@ const handleSubmit = (event) => {
     <div className={style.container}>
       <form className={style.formulario} onSubmit={handleSubmit} ref={formRef}>
         <div className={style.grid}>
-                <div className={style.div1}>
-
-                    <label htmlFor="name">Nombre: </label>
-                    <input placeholder='Escriba un Nombre' type="text" name="nombre" value={input.nombre} onChange={handleInputChange} onBlur={handleInputChange}/>
-                        {errorMessages.nombre && <p>{errorMessages.nombre}</p>}
+          <div className={style.div1}>
+            <label htmlFor="name">Nombre: </label>
+            <input placeholder='Escriba un Nombre' type="text" name="nombre" value={input.nombre} onChange={handleInputChange} onBlur={handleInputChange}/>
+                {errorMessages.nombre && <p>{errorMessages.nombre}</p>}
                         
-                    <label htmlFor="image">Imagen: </label>
-                    <input placeholder='Inserte una URL' type="url" name="image" value={input.image} onChange={handleInputChange} onBlur={handleInputChange}/>
-                        {errorMessages.image && <p>{errorMessages.image}</p>}   
+            <label htmlFor="image">Imagen: </label>
+            <input placeholder='Inserte una URL' type="url" name="image" value={input.image} onChange={handleInputChange} onBlur={handleInputChange}/>
+                {errorMessages.image && <p>{errorMessages.image}</p>}   
+        
+            <label htmlFor="fechalanzamiento">Fecha de Lanzamiento  (Formato YYYY-MM-DD): </label>
+            <input placeholder='YYYY-MM-DD' type="text" name="fechaLanzamiento" value={input.fechaLanzamiento} onChange={handleInputChange} onBlur={handleInputChange}/>
+                {errorMessages.fechaLanzamiento && <p>{errorMessages.fechaLanzamiento}</p>}
 
-                    
-                    <label htmlFor="fechalanzamiento">Fecha de Lanzamiento  (Formato YYYY-MM-DD): </label>
-                    <input placeholder='YYYY-MM-DD' type="text" name="fechaLanzamiento" value={input.fechaLanzamiento} onChange={handleInputChange} onBlur={handleInputChange}/>
-                        {errorMessages.fechaLanzamiento && <p>{errorMessages.fechaLanzamiento}</p>}
+          </div>
+          <div className={style.div2}>
+            <label htmlFor="rating">Rating: </label>
+            <input placeholder='Ej: 3,54' type="number" name="rating" step="0.01" value={input.rating} onChange={handleInputChange} onBlur={handleInputChange}/>
+                {errorMessages.rating && <p>{errorMessages.rating}</p>}
 
-                </div>
-
-                <div className={style.div2}>
-
-                    <label htmlFor="rating">Rating: </label>
-                    <input placeholder='Ej: 3,54' type="number" name="rating" step="0.01" value={input.rating} onChange={handleInputChange} onBlur={handleInputChange}/>
-                        {errorMessages.rating && <p>{errorMessages.rating}</p>}
-
-                    <label  htmlFor="descripcion">Descripcion: </label>
-                    <textarea placeholder='este es un videojuego muy bueno ya que tiene muchas funcionalidades y cosas nuevas experimentales....' className={style.descripcion} type="text" name="descripcion" value={input.descripcion} onChange={handleInputChange} onBlur={handleInputChange}/>
-                    {errorMessages.descripcion && <p>{errorMessages.descripcion}</p>}
-                </div>
-                <div className={style.div3}>
-  <label htmlFor="plataformas">Plataformas: </label>
-  <div className={style.checktipes}>
-    {plataformas.map((plataforma) => (
-      <label key={plataforma}>
-        <input
-          type="checkbox"
-          name="plataforma"
-          value={plataforma}
-          checked={input.plataformas.includes(plataforma)}
-          onChange={handlePlataformaChange}
-        />
-        {plataforma.charAt(0).toUpperCase() + plataforma.slice(1)}
-      </label>
-    ))}
-  </div>
-  {platformError && <p>{platformError}</p>}
-</div>
-
-<div className={style.div4}>
-  <label htmlFor="genre">Género(s): </label>
-  <div className={style.checktipes}>
-    {genres.map((genre) => (
-      <label key={genre} className={style.checkboxes}>
-        <input
-          type="checkbox"
-          name="genre"
-          value={genre}
-          checked={input.genero.includes(genre)}
-          onChange={handleGenreChange}
-        />
-        {genre.charAt(0).toUpperCase() + genre.slice(1)}
-      </label>
-    ))}
-  </div>
-  {genreError && <p>{genreError}</p>}
-</div>
-
+            <label  htmlFor="descripcion">Descripcion: </label>
+            <textarea placeholder='este es un videojuego muy bueno ya que tiene muchas funcionalidades y cosas nuevas experimentales....' className={style.descripcion} type="text" name="descripcion" value={input.descripcion} onChange={handleInputChange} onBlur={handleInputChange}/>
+                {errorMessages.descripcion && <p>{errorMessages.descripcion}</p>}
+          </div>
+          <div className={style.div3}>
+            <label htmlFor="plataformas">Plataformas: </label>
+            <div className={style.checktipes}>
+              {plataformas.map((plataforma) => (
+                  <label key={plataforma}>
+                    <input
+                      type="checkbox"
+                      name="plataforma"
+                      value={plataforma}
+                      checked={input.plataformas.includes(plataforma)}
+                      onChange={handlePlataformaChange}
+                    />
+                      {plataforma.charAt(0).toUpperCase() + plataforma.slice(1)}
+                  </label>
+              ))}
+            </div>      
+                {platformError && <p>{platformError}</p>}
+          </div>
+          <div className={style.div4}>
+            <label htmlFor="genre">Género(s): </label>
+            <div className={style.checktipes}>
+              {genres.map((genre) => (
+                <label key={genre} className={style.checkboxes}>
+                  <input
+                    type="checkbox"
+                    name="genre"
+                    value={genre}
+                    checked={input.genero.includes(genre)}
+                    onChange={handleGenreChange}
+                  />
+                  {genre.charAt(0).toUpperCase() + genre.slice(1)}
+                </label>
+              ))}
+            </div>
+                {genreError && <p>{genreError}</p>}
+          </div>
         </div>
-        <button
-  type="submit"
-  id="submit-button"
-  disabled={
-    Object.values(errorMessages).some((message) => message !== '') ||
-    Object.values(input).some((value) => value === '') ||
-    input.genero.length === 0 ||
-    input.plataformas.length === 0
-  }
->
-  Crear Videogame
-</button>
-
+          <button type="submit" id="submit-button" disabled={ Object.values(errorMessages).some((message) => message !== '') || Object.values(input).some((value) => value === '') || input.genero.length === 0 || input.plataformas.length === 0 }>Crear Videogame</button>
       </form>
       <div className={style.presentacion}>
         <div className={style.card}>
-
-            <div className={style.front}>
-                <div className={style.imageContainer}>
-                    <img
-                        src={input.image}
-                        alt={input.image}
-                        className={style.image}
-                        />
-                </div>
-                    <h2 className={style.name}>{input.nombre}</h2>
-                </div>
-                <div>
-                    {input.genero.map((genre) => (
-                        <span key={genre} className={style.tipo}>
-                        {genre}
-                        </span>
-                    ))}
-                </div>
-                </div>
+          <div className={style.front}>
+            <div className={style.imageContainer}>
+              <img src={input.image} alt={input.image} className={style.image} />
             </div>
-
+              <h2 className={style.name}>{input.nombre}</h2>
+          </div>
+          <div>
+            {input.genero.map((genre) => (
+              <span key={genre} className={style.tipo}>{genre}</span>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
