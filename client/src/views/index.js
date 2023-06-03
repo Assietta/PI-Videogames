465ba029f5d916157ee922706a207e9736a06bee
Jeremias Brussino <jeremiasbrussino@gmail.com>
2023-06-02 21:56:47 -0300

    actualizar

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home/Home.jsx";
-import LandingPage from "./Landing/LandingPage";
-import Detail from "./Detail/Detail";
-import Form from "./Form/Form";
+import LandingPage from "./Landing/LandingPage.jsx";
+import Detail from "./Detail/Detail.jsx";
+import Form from "./Form/Form.jsx";
 
 export { Home, Detail, Form, LandingPage }
