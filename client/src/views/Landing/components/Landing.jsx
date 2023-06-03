e28827fa176f305e944d0b268e35e3bc3a04ecbf
Jeremias Brussino <jeremiasbrussino@gmail.com>
2023-06-02 22:52:16 -0300

    css

--- client/src/views/Landing/components/Landing.jsx
+++ client/src/views/Landing/components/Landing.jsx
@@ -8,11 +8,17 @@ const Landing = () => {
       <p className={style.subtitle}>
         Creado y Diseñado por <a href="https://www.linkedin.com/in/assietta/" className={style.link}>Jeremias Brussino</a>
       </p>
+      <div className={style.imagecontainer}>
+        <img src="https://areajugones.sport.es/wp-content/uploads/2022/06/the-last-of-us-remake-1.webp" alt="Imagen 1" />
+        <img src="https://areajugones.sport.es/wp-content/uploads/2022/06/the-last-of-us-remake-1.webp" alt="Imagen 2" />
+        <img src="https://areajugones.sport.es/wp-content/uploads/2022/06/the-last-of-us-remake-1.webp" alt="Imagen 3" />
+      </div>
       <div className={style["button-container"]}>
         <Link to="/Home" className={style.button}>Entrar</Link>
       </div>
     </div>
   );
+  
 };
 
 export default Landing;
