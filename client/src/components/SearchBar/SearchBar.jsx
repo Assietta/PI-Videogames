88a6c70327ea7eb17441d6dd84475ee0e5f41011
Jeremias Brussino <jeremiasbrussino@gmail.com>
2023-06-05 22:41:12 -0300

    voice

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getName } from "../../redux/actions";
 import styles from "./SearchBar.module.css";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState("");
   const videogames = useSelector((state) => state.videogames);
+  const { transcript, resetTranscript } = useSpeechRecognition();
 
   useEffect(() => {
-    if (name.trim() !== "") {
-      dispatch(getName(name));
-    } 
-  }, [name, dispatch]);
+    if (transcript.trim() !== "") {
+      dispatch(getName(transcript));
+      console.log(transcript);
+      resetTranscript();
+    }
+  }, [transcript, dispatch, resetTranscript]);
 
   const handleInputChange = (event) => {
     const inputValue = event.target.value;
@@ -24,7 +28,11 @@ const SearchBar = () => {
     dispatch(getName(name));
   };
 
-  
+  const handleVoiceInput = () => {
+    SpeechRecognition.startListening({ language: ["es-ES", "en-US"] });
+
+  };
+
   return (
     <form className={styles.searchBar} onSubmit={handleSearch}>
       <input
@@ -34,6 +42,9 @@ const SearchBar = () => {
         onChange={handleInputChange}
         placeholder="Search Videogame..."
       />
+      <button type="button" onClick={handleVoiceInput}>
+        Voice Search
+      </button>
       {videogames && videogames.name && (
         <div className={styles.result}>
           <h3>{videogames.name}</h3>
